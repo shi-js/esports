dc7c029f90326cf3b6c8549f14d0b7f7513f37c6
akshat <akshat.b2007@gmail.com>
2020-12-02 12:46:57 +0530

    google sign up integrated

--- app/src/main/java/com/example/esportsarena/activities/SignInActivity.kt
+++ app/src/main/java/com/example/esportsarena/activities/SignInActivity.kt
@@ -1,7 +1,6 @@
 package com.example.esportsarena.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.WindowManager
@@ -9,12 +8,17 @@ import android.widget.Toast
 import com.example.esportsarena.R
 import com.example.esportsarena.models.User
 import com.example.projemanage.activities.BaseActivity
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
+
 
 class SignInActivity : BaseActivity() {
 
     private lateinit var auth: FirebaseAuth
+
+
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,17 +27,23 @@ class SignInActivity : BaseActivity() {
 
         auth = FirebaseAuth.getInstance()
 
+
+
+
+
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
         btnSignIn.setOnClickListener {
             checkUser() }
+
+
     }
 
     fun signInSuccess(user: User){
         hideProgressDialog()
-        startActivity(Intent(this,MainActivity::class.java))
+        startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
 
@@ -50,7 +60,7 @@ class SignInActivity : BaseActivity() {
 
         toolbar_sign_in_activity.setNavigationOnClickListener { onBackPressed() }
     }
-    private fun validateForm( email: String, password: String) : Boolean {
+    private fun validateForm(email: String, password: String) : Boolean {
 
         return when {
 
@@ -73,23 +83,40 @@ class SignInActivity : BaseActivity() {
         val email: String = edtEmailSignIn.text.toString().trim{ it <= ' '}
         val password: String = edtPasswordSignIn.text.toString().trim{ it <= ' '}
 
-        if (validateForm(email,password)){
+        if (validateForm(email, password)){
             showprogressDialog(resources.getString(R.string.please_wait))
-            auth.signInWithEmailAndPassword(email,password).addOnCompleteListener{
-                    task->
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener{ task->
                 hideProgressDialog()
                 if (task.isSuccessful){
-                    Toast.makeText(this,"Sign In Successful",
-                        Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        this, "Sign In Successful",
+                        Toast.LENGTH_SHORT
+                    ).show()
                     val user = auth.currentUser
-                    startActivity(Intent(this,MainActivity::class.java))
+                    startActivity(Intent(this, MainActivity::class.java))
 
 
                 }else{
-                    Toast.makeText(this,"Sign In Failed",
-                        Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        this, "Sign In Failed",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
         }
+    }
+
+
+
+
+
+
+
+
+
+
+    companion object {
+        private const val TAG = "GoogleActivity"
+        private const val RC_SIGN_IN = 9001
     }
 }
--- app/src/main/java/com/example/esportsarena/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/esportsarena/activities/SignUpActivity.kt
@@ -1,18 +1,32 @@
 package com.example.esportsarena.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
 import com.example.esportsarena.Firebase.FirestoreClass
 import com.example.esportsarena.R
 import com.example.esportsarena.models.User
 import com.example.projemanage.activities.BaseActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : BaseActivity() {
+
+    lateinit var gso: GoogleSignInOptions
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    val RC_SIGN_IN: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -22,6 +36,45 @@ class SignUpActivity : BaseActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        val signIn = findViewById<View>(R.id.GsignIn) as SignInButton
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        signIn.setOnClickListener {
+                view: View? -> GsignIn()
+        }
+    }
+
+    private fun GsignIn() {
+
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleResult (task)
+        }else {
+            Toast.makeText(this, "Problem in execution order :(", Toast.LENGTH_LONG).show()
+        }
+    }
+    private fun handleResult (completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
+            if (account != null) {
+                GregisterUser(account)
+                
+                finish()
+            }
+        } catch (e: ApiException) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
+        }
     }
 
     fun userRegisteredSuccess(){
@@ -99,4 +152,33 @@ class SignUpActivity : BaseActivity() {
                 }
         }
     }
+
+    private fun GregisterUser(account: GoogleSignInAccount){
+        val name: String? = account.displayName
+        val email: String? = account.email
+        val pass: Int? = 123456
+
+        if (email != null) {
+            FirebaseAuth.getInstance()
+                .createUserWithEmailAndPassword(email, pass.toString())
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val firebaseUser: FirebaseUser = task.result!!.user!!
+                        val registeredEmail = firebaseUser.email!!
+
+
+                        val user = name?.let { User(firebaseUser.uid, it,registeredEmail) }
+
+                        if (user != null) {
+                            FirestoreClass().registerUser(this,user)
+                        }
+                    } else {
+                        Toast.makeText(
+                            this,
+                            task.exception!!.message, Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+}
 }
